5f3a09ed424e4609b79075d32ddda0d3e3260991
Winton Welsh <mail@wintoni.us>
2016-06-10 11:25:12 -0700

    Revert "Fixing specs"
    This reverts commit 713491efbbcc097637ba83e22c2c0e72ceea546c.

--- spec/integrate/industry-standard-io.spec.js
+++ spec/integrate/industry-standard-io.spec.js
@@ -135,7 +135,7 @@ describe("standard_io", () => {
     let test = makeTest().base(base)
     let output = test().hello()
     
-    expect(output.value).toEqual({})
+    expect(output.value).toEqual(undefined)
     
     output.catch(e => {
       expect(e).toEqual("rejected")
@@ -151,7 +151,7 @@ describe("standard_io", () => {
     let test = makeTest().base(base)
     let output = test().hello()
     
-    expect(output.value).toEqual({})
+    expect(output.value).toEqual(undefined)
     
     output.catch(e => {
       expect(e).toEqual("rejected")
